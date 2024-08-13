380bf69d6044ecd415c1971c2a26ab6cb6c5ef78
Ajobiewe Joseph <joseph4hacking@gmail.com>
2024-08-13 14:44:51 +0100

    fetch the suscribers of a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A module containing a function that uses the Reddit API
+to get the number of subscribers for a given sub-reddit
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    This function takes in a subreddit and returns the total
+    number of subscribers it has (Not active users!)
+    """
+
+    endpoint = f'https://www.reddit.com/r/{subreddit}/about.json'
+    header = {'User-Agent': 'Ubuntu/20.04 ALXSE /u/jossyboy2580'}
+
+    response = requests.get(
+                            endpoint,
+                            headers=header,
+                            allow_redirects=False
+                            )
+    if response.status_code > 399:
+        return 0
+    return response.json()['data'].get('subscribers', 0)
